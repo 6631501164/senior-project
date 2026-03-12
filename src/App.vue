d603f355423142b6c412dc7fffe385aee9c8dc09
6631501164 <6631501164@lamduan.mfu.ac.th>
2026-03-12 11:11:00 +0700

    start senior project software house system

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Software House Management System</h1>
+    <p>Welcome to our project system</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
